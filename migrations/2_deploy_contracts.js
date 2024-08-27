91c5a577e9ff192c518b79ab4c2cc3855159730e
Pavani chitturi <pavanichitturi24@gmail.com>
2024-08-27 21:37:54 +0530

    Add files via upload

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,1 +1,6 @@
+const CoinFlip = artifacts.require("CoinFlip");
 
+module.exports = async function(deployer, network, accounts) {
+  const gasPrice = web3.utils.toWei('35', 'gwei'); // Adjust this value
+  await deployer.deploy(CoinFlip, { gas: 5500000, gasPrice: gasPrice });
+};
